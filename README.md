7bc4ce01f5399bb55540fd4f62c1899b275508e6
abddulrahmaanHussein <a.hussein@fis.ps>
2021-10-03 14:33:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Huddle Landing Page (React & Styled Components)
+
+Project from my styled components crash course
+
+## Usage
+
+```
+npm install
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Card from './components/Card';
+import { Container } from './components/styles/Container.styled';
+import GlobalStyles from './components/styles/Global';
+import content from './content';
+
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  },
+  mobile: '768px'
+}
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {
+            content.map((item, index) => (
+              <Card item={item} key={index}/>
+            ))
+          }
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
